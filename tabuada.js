90a27fe0f2f163c806c760f5970f50a6da01ed26
anisio silva santos <allangabrielrvsantos@yahoo.com.br>
2021-06-25 17:09:42 -0300

    'tabuada'

--- tabuada.js
+++ tabuada.js
@@ -1,7 +1,7 @@
-let numero=prompt("Digite um número inteiro");
-let numero2= Número(numero);
+let numero= prompt("Digite um número inteiro positivo");
+let numero2= Number(numero);
 
-para (let i = 0; <= 10; i=i+1)
+for(let i = 0; i <= 10; i=i+1)
 {
-    alerta('${numero2} X ${i}= ${numero2*i});
+    alert(`${numero2} X ${i}= ${numero2*i}`);
 }
